1697f0fd19d4fd3d22d6579f155d7a3367130b5d
alexciccarese <alex.ciccarese@gmail.com>
2025-02-11 14:03:00 +0100

    add multiple of 3 in Fizz

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 // scrivere programma che stampi da 1 a 100 
+// aggiungere che a ogni multiplo di 3 esca fuori la scritta "Fizz" al posto del numero
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+    
+    if (i % 3 === 0) {
+      console.log("Fizz");
+      
+    } else {
+      console.log(i);
+
+    }
     
 }
