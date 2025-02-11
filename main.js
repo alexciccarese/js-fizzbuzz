e0326ffe0a183ecd35e569e11a178b12dab93d1d
alexciccarese <alex.ciccarese@gmail.com>
2025-02-11 14:10:06 +0100

    add multiple of 5 in Buzz

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 // scrivere programma che stampi da 1 a 100 
 // aggiungere che a ogni multiplo di 3 esca fuori la scritta "Fizz" al posto del numero
+// aggiungere che a ogni multiplo di 5 esca fuori la scritta "Buzz" al posto del numero
 
 for (let i = 1; i <= 100; i++) {
     
     if (i % 3 === 0) {
       console.log("Fizz");
       
-    } else {
-      console.log(i);
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+      
+    } 
 
-    }
-    
+    console.log(i);
 }
