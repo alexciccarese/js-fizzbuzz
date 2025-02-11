00e0d08dc42132aa37e641690342968977aa6040
alexciccarese <alex.ciccarese@gmail.com>
2025-02-11 14:27:12 +0100

    add numbrs divisible for 3 and 5

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // scrivere programma che stampi da 1 a 100 
 // aggiungere che a ogni multiplo di 3 esca fuori la scritta "Fizz" al posto del numero
 // aggiungere che a ogni multiplo di 5 esca fuori la scritta "Buzz" al posto del numero
+// aggiungere che al posto dei numero moltiplicabili sia per 3 che per 5 esca la scritta "FizzBuzz"
 
 for (let i = 1; i <= 100; i++) {
     
@@ -10,7 +11,12 @@ for (let i = 1; i <= 100; i++) {
     } else if (i % 5 === 0) {
       console.log("Buzz");
       
-    } 
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+      
+    } else {
 
-    console.log(i);
+      console.log(i);
+    }
+
 }
